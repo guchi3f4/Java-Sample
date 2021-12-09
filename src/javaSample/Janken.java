f8b8056f600f9a38a325bcf3302dfbc43984f1f2
guchi3f4 <guchi3f4@gmail.com>
2021-12-09 17:30:50 +0900

    Referee

--- src/javaSample/Janken.java
+++ src/javaSample/Janken.java
@@ -3,14 +3,26 @@ package javaSample;
 import java.util.Random;
 
 class Player {
-	String name;
-	String handStatus;
+	private String name;
+	private String handStatus;
 	
-	Player(String name) {
+	public Player(String name) {
 		this.name = name;
 	}
 	
-	void makeHandStatus() {
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getHand() {
+		return handStatus;
+	}
+	
+	public void makeHandStatus() {
 		String[] hands = {"グー", "チョキ", "パー"};
 		Random r = new Random();
 		this.handStatus = hands[r.nextInt(3)];
@@ -18,35 +30,43 @@ class Player {
 }
 
 class Referee {
-	String name;
+	private String name;
 	
-	Referee(String name) {
+	public Referee(String name) {
 		this.name = name;
 	}
 	
-	void startJanken() {
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void startJanken() {
 		System.out.println("じゃんけん・・・ぽん！！！！！");
 	}
 	
-	void chackHand(Player p) {
-		System.out.println(p.name + "さんの手：" + p.handStatus);
+	public void chackHand(Player p) {
+		System.out.println(p.getName() + "さんの手：" + p.getHand());
 	}
 	
-    void judegJunken(Player p1, Player p2) {
+	public void judegJunken(Player p1, Player p2) {
     	System.out.println("結果は");
 		
-		if(p1.handStatus == p2.handStatus) {
+		if(p1.getHand() == p2.getHand()) {
 			System.out.println("あいこ！勝負つかず！");
 			
-		} else if(    (p1.handStatus == "グー" && p2.handStatus == "チョキ")
-				   || (p1.handStatus == "チョキ" && p2.handStatus == "パー")
-				   || (p1.handStatus == "パー" && p2.handStatus == "グー") ) {
-			System.out.println(p1.name + "さんの勝利");
+		} else if(    (p1.getHand() == "グー" && p2.getHand() == "チョキ")
+				   || (p1.getHand() == "チョキ" && p2.getHand() == "パー")
+				   || (p1.getHand() == "パー" && p2.getHand() == "グー") ) {
+			System.out.println(p1.getName() + "さんの勝利");
 			
-		} else if(    (p1.handStatus == "グー" && p2.handStatus == "パー")
-				   || (p1.handStatus == "チョキ" && p2.handStatus == "グー")
-				   || (p1.handStatus == "パー" && p2.handStatus == "チョキ") ) {
-			System.out.println(p2.name + "さんの勝利");
+		} else if(    (p1.getHand() == "グー" && p2.getHand() == "パー")
+				   || (p1.getHand() == "チョキ" && p2.getHand() == "グー")
+				   || (p1.getHand() == "パー" && p2.getHand() == "チョキ") ) {
+			System.out.println(p2.getName() + "さんの勝利");
 		}
 	}
 }
