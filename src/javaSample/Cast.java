cb22fc07bcafe051bc5cb6768e8295bf1d9413c3
guchi3f4 <guchi3f4@gmail.com>
2021-12-14 19:39:21 +0900

    Cast

---
+++ src/javaSample/Cast.java
@@ -0,0 +1,54 @@
+package javaSample;
+
+class Animal {
+	String name;
+}
+
+class Dog extends Animal {	
+}
+
+public class Cast {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		byte calc1_1 = 1;
+		short calc1_2 = 2;
+		String calc1_3 = "00000000003";
+		String answer1 = calc1_1 + calc1_2 + calc1_3;
+		System.out.println(answer1);
+		
+		byte calc2_1 = 4;
+		double calc2_2 = 5;
+		int calc2_3 = 6;
+		double answer2 = calc2_1 + calc2_2 + calc2_3;
+		System.out.println(answer2);
+		
+		short calc3_1 = 7;
+		String calc3_2 = "8";
+		int calc3_3 = 9;
+		int answer3 = calc3_1 + Integer.parseInt(calc3_2) + calc3_3;
+		System.out.println(answer3);
+		
+		int calc4_1 = 10;
+		long calc4_2 = 11;
+		long answer4 = calc4_1 + calc4_2;
+		System.out.println(answer4);
+		
+		Dog dog = new Dog();
+		//アップキャスト
+		Animal animal = dog;
+		//ダウンキャスト
+		dog = (Dog) animal;
+		animal.name = "犬";
+		
+		//例外が表示される記述
+//		Animal animal = new Animal();
+//		Dog dog = (Dog) animal;
+//		dog.name = "犬";
+		
+		System.out.println(dog.name);
+		
+
+	}
+
+}
