d1922f89f0e0eee2c0f1a6386d1e05603329deb0
guchi3f4 <guchi3f4@gmail.com>
2021-12-25 18:25:14 +0900

    ポリモーフィズム

---
+++ src/javaSample/SampleA.java
@@ -0,0 +1,38 @@
+package javaSample;
+
+class A {
+	String val = "a" ;
+	void print() {
+		System.out.println(val);
+	}
+}
+
+class B extends A {
+//	B() {
+//		this.val = "c";
+//	}
+	
+	String val = "b" ;
+}
+
+public class SampleA {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		A a_a = new A();
+		A a_b = new B();
+		//B b_a = (B) new A(); スーパークラスからサブクラスへの型変換はエラーが起きる
+		B b_b = new B();
+		
+		System.out.println(a_a.val);
+		System.out.println(a_b.val);
+		//System.out.println(b_a.val);
+		System.out.println(b_b.val);
+		
+		a_a.print();
+		a_b.print();
+		//b_a.print();
+		b_b.print();
+	}
+
+}
