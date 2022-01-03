d2447677817a6800cfa19cdee6a40e02d4e6c7b2
guchi3f4 <guchi3f4@gmail.com>
2022-01-03 22:17:46 +0900

    sample

--- src/javaSample/SampleB.java
+++ src/javaSample/SampleB.java
@@ -7,6 +7,10 @@ package javaSample;
 class Sample1 {
 	String name;
 	
+	public String getName() {
+		return name;
+	}
+	
 	public void test() {
 		System.out.println("テスト1");
 	}
@@ -15,6 +19,9 @@ class Sample1 {
 		return a;
 	}
 	
+	public void modify(Sample1 a) {
+		a.name = "太郎";
+	}
 	
 }
 
@@ -34,8 +41,14 @@ class Sample2 extends Sample1 {
 	public int chack(short b) {
 		return b * 3;
 	}
+}
+
+class Sample3 extends Sample1 {
+	String name;
 	
-	
+//	public String getName() {
+//		return name;
+//	}
 }
 
 public class SampleB {
@@ -71,6 +84,31 @@ public class SampleB {
 			System.out.println(i++);
 			j += i;
 		}
+		
+//		Sample1[] sam = { new Sample1(), new Sample2() };
+		Sample1[] str = new Sample1[1]; 
+		System.out.println(str[0]);
+		
+		//Sample1[] sam2 = new Sample1[1];
+		//System.out.println(sam2[0].name); //例外が発生する
+		
+		Integer inte1 = (int)2.7; 
+		int int1 = (int)1.9;
+		inte1 += int1;
+		System.out.println(inte1);
+		
+		Sample1 a3 = new Sample1();
+		Sample2 b3 = new Sample2();
+		a3.modify(b3);
+		System.out.println(a3.name);
+		System.out.println(b3.name);
+		
+		Sample1 a4 = new Sample1();
+		Sample3 b4 = new Sample3();
+		b4.name = "次郎";
+		System.out.println(a4.getName());
+		System.out.println(b4.getName());
+		System.out.println(b4.name);
 	}
 
 }
